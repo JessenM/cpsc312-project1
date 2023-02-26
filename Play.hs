27ffbbb9d2f46243f5ab6149d9228a94407c4ddc
eritter100 <eritter100@outlook.com>
2023-02-25 22:27:19 -0800

    Added more direction+fix digit bug
    when giving action, if give number larger than 10, it reads first digit only, made so that you can only give single digit numbers as action

--- Play.hs
+++ Play.hs
@@ -14,16 +14,19 @@ import DrawBoard
 
 import System.IO
 import Text.Read   (readMaybe)
+import Data.List
 
 type TournammentState = (Int,Int,Int)   -- wins, losses, ties
 
 
 play :: Game -> State -> Player -> TournammentState -> IO TournammentState
 
+game_Directions = "\n Welcome to Ultimate Tic-Tac-Toe! \n Your inputs on the board are as such: \n "++(intercalate ("\n"++" ---+---+--"++"\n") ["0 | 1 | 2 ", " 3 | 4 | 5 ", " 6 | 7 | 8 "])
+
 play game start_state opponent ts =
   let (wins, losses,ties) = ts in
   do
-      putStrLn ("Tournament results: "++ show wins++ " wins "++show losses++" losses "++show ties++" ties")
+      putStrLn (game_Directions++"\n \n \n"++"Tournament results: "++ show wins++ " wins "++show losses++" losses "++show ties++" ties")
       putStrLn "Who starts? 0=you, 1=computer, 2=exit."
       line <- getLine
       if line == "0"
